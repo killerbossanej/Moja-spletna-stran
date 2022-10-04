95109c45c962de29aa338d6ab0349f7b4196aea8
killerbossanej <74080259+killerbossanej@users.noreply.github.com>
2022-10-04 13:10:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function toggleMobileMenu(menu){
+    menu.classList.toggle('open');
+}
+
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
